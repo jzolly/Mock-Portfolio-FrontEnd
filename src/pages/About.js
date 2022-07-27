1fb5d9095f9d913cdee1c28a70c9d8e2ca3f0f50
Jessie Zollinger <jzolly@hotmail.com>
2022-07-27 15:11:43 -0400

    styled about and added tech skills icon

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material";
 import { useState, useEffect } from "react";
 
 function About(props) {
@@ -21,38 +22,56 @@ function About(props) {
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
-    <div className="aboutContainer">
-      <div className="professionalInfo">
-        <img
-          className="headShot"
-          src={about.headshot}
-          alt={about.name}
-          height="450px"
-        />
-        <h1>{about.name}</h1>
-        <a
-          href={about.resume}
-          style={{ color: "#af8fc4", textDecoration: "none" }}
-        >
-          <p>Resume</p>
-        </a>
-        <p>{about.email}</p>
-      </div>
+    <div>
+      <Grid container className="aboutContainer">
+        <Grid item xs={12} md={4} className="leftDiv">
+          <img
+            className="headShot"
+            src={about.headshot}
+            alt={about.name}
+            height="450px"
+          />
+          <a
+            href={about.resume}
+            style={{ color: "#af8fc4", textDecoration: "none" }}
+          >
+            <p>Resume</p>
+          </a>
+          <h3>Tech Skills</h3>
+          <img className="skillIcon" src="https://img.icons8.com/fluency/48/000000/javascript.png" alt="JS icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/color-glass/48/000000/python.png" alt="python icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/color/48/000000/html-5--v1.png" alt="html5 icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/color/48/000000/css3.png" alt="css3"/>
+          <img className="skillIcon" src="https://img.icons8.com/windows/32/000000/django.png" alt="django icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/color/48/000000/mongodb.png" alt="mongoDB icon"/>
+          <img className="skillIcon" src="https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png" alt="express icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/plasticine/100/000000/react.png" alt="react icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/fluency/48/000000/node-js.png" alt="node.js icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/ios/50/000000/postgreesql.png" alt="postgreSQL icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/external-tal-revivo-tritone-tal-revivo/32/000000/external-invision-is-the-digital-product-design-platform-logo-tritone-tal-revivo.png" alt="inVision Icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/fluency/48/000000/procreate.png" alt="proCreate Icon"/>
+          <img className="skillIcon" src="https://img.icons8.com/color/48/000000/figma--v1.png" alt="Figma Icon"/>
+          <br/>
+          <a target="_blank" href="https://icons8.com/icon/AksudKrBQryM/django" style={{ color: '#c9e265'}}>Skill icons by Icons8</a>
+        </Grid>
+        <Grid item xs={12} md={8} className="right">
+          <h1 className="aboutHeader">It's me...</h1>
+          <div className="bio">
+            <h3>Location</h3>
+            <p>{about.location}</p>
+            <h3>Bio</h3>
+            <p>{about.bio1}</p>
+            <br />
+            <p>{about.bio2}</p>
+            <br />
+            <p>{about.bio3}</p>
+            <br />
+            <h3>Interests</h3>
+            <p>{about.interests}</p>
+          </div>
+        </Grid>
+      </Grid>
       <hr />
-      <h2>It's me...</h2>
-      <div className="bio">
-        <p>{about.bio1}</p>
-        <br />
-        <p>{about.bio2}</p>
-        <br />
-        <p>{about.bio3}</p>
-        <br />
-        <h3>Location</h3>
-        <p>{about.location}</p>
-        <img src={about.photoAdvneture} alt={about.name} height="450px" />
-        <h3>Interests</h3>
-        <p>{about.interests}</p>
-      </div>
     </div>
   );
 
